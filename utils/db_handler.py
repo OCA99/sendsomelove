73614d03e2beab91988b67d603fdda0d88cfe9df
PolBaladas <polbaladasluna@gmail.com>
2018-01-18 12:33:23 +0100

    Database. First Model, still WIP

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -1,4 +1,30 @@
+from random import randint
 from sqlalchemy import or_
 from main import app,db
 from .models import *
 from .values import *
+
+def IDExists(id):
+    query = Site.query.filter_by(public_id=id).first()
+    return (query == None)
+
+
+def createID(length=6):
+    ret = ""
+    i = 0
+    for i in range(length):
+        n = randint(0, 35)
+        if n > 25:
+            n += 22
+        else:
+            n += 97
+        ret = ret[:i] + str(chr(n)) + ret[i + 1:]
+    return ret
+
+
+def newSite(title):
+	public_id = createID(8)
+	site = Site(title, public_id)
+	db.session.add(site)
+	db.session.commit()
+	return public_id
--- utils/models.py
+++ utils/models.py
@@ -1,1 +1,10 @@
 from main import app, db
+
+class Site(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	public_id = db.Column(db.String(20))
+	title = db.Column(db.Text)
+
+	def __init__(self, title, public_id):
+		self.public_id = public_id
+		self.title = title
